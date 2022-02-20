61dcf6c93f32bfbad914f0d9fc090d8dc1854b5d
H-M-Masud <hmmasud163@gmail.com>
2022-02-20 15:35:25 +0600

    filter find

---
+++ filter-find.js
@@ -0,0 +1,23 @@
+const numbers = [5, 13, 7, 41, 39, 79, 30, 5, 2, 19];
+
+const bigNumber = numbers.filter(number => number > 20);
+const smallNumbers = numbers.filter(number => number < 10);
+// console.log(smallNumbers)
+
+
+const products = [
+    {name: 'water bottle', price: 50, color: 'yellow'},
+    {name: 'mobile phone', price: 15000, color: 'black'},
+    {name: 'smart watch', price: 1500, color: 'black'},
+    {name: 'sticky note', price: 30, color: 'pink'},
+    {name: 'water glass', price: 3, color: 'white'},
+];
+
+const expensive = products.filter(product => product.price > 100);
+// console.log(expensive)
+
+const blacks = products.filter(product => product.color == 'pink');
+// console.log(blacks)
+
+const whiteItem = products.find(product => product.color == 'pink');
+console.log(whiteItem)
